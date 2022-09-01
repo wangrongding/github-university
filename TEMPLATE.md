503206287cb21477dcaf3d0b22aec1b11cf799ed
wangrongding <wangrongding@qq.com>
2022-09-01 15:22:50 +0800

    chore: 初始化仓库

---
+++ TEMPLATE.md
@@ -0,0 +1,11 @@
+# Github University
+
+Aggregate quality learning resources, useful tools, interesting projects in github...
+
+汇总 github 中优质的学习资源，好用的工具，有趣的项目...
+
+## 前端
+
+| name/url              | desc                      | stars |
+| --------------------- | ------------------------- | ----- |
+| [github-university]() | Haha, this is the project | 9999  |
---
+++ request.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+// 创建axios实例
+const request = axios.create({
+  timeout: 200000, // 请求超时时间
+});
+// request请求拦截器
+request.interceptors.request.use(
+  (config) => {
+    const { data = {}, method } = config;
+    if (method === "post") {
+      config.data = data.data;
+    }
+    // get请求转参数key为params
+    if (method === "get" || method === "delete") {
+      config.params = data;
+    }
+    if (method === "put") {
+      config.data = { ...data.data };
+    }
+    return config;
+  },
+  (error) => error
+);
+
+// 请求成功回调
+function successCallback(response) {
+  // console.log(response);
+  return Promise.resolve(response);
+}
+// 请求错误回调
+function errorCallback(error) {
+  console.log(error);
+  return Promise.reject(error);
+}
+// response返回拦截器
+request.interceptors.response.use(successCallback, errorCallback);
+
+export default request;
